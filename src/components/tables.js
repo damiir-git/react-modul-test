2e5e50dcc7e2ad02a727a71482dab0c6c768b868
damiir <damiir@gmail.com>
2020-03-25 00:57:51 +0500

    Добавил Axios + json-server

---
+++ src/components/tables.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+class Table extends React.Component {
+    render() {
+        return <div className="Table">{this.props.children}</div>
+    }
+}
+
+class Row extends React.Component {
+    render() {
+        return <div className="Row">{this.props.children}</div>
+    }
+}
+
+class Head extends React.Component {
+    render() {
+        return <div className="Head">{this.props.children}</div>
+    }
+}
+
+class Item extends React.Component {
+    render() {
+        return <div className="Item">{this.props.children}</div>
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        return (
+            <Head>
+                {this.props.heads.map((item) => {
+                    return (
+                        <Item key={item.id}>{item.name}</Item>
+                    );
+                })}
+            </Head>
+        )
+    }
+}
+
+export {
+    Table,
+    Row,
+    Item,
+    Header
+};
---
+++ src/store/actions.js
@@ -0,0 +1,8 @@
+const READ_ITEM = 'READ_ITEM';
+const actionReadItem = {type: READ_ITEM, payload: null};
+
+const EDIT_ITEM = 'EDIT_ITEM';
+const actionEditItem = {type: EDIT_ITEM, payload: null};
+
+const DEL_ITEM = 'DEL_ITEM';
+const actionDelItem = {type: DEL_ITEM, payload: null};
